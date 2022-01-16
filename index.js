5378bd817c30fa3dcbd9e78013df16dfb6c7d3bf
Miller Johnston <millerjohnston@pop-os.localdomain>
2022-01-15 16:18:54 -0800

    Commit after beginning backend and frontend work

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./src/App";
+import App from "./src/Client/App"
+
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+
+
+// reportWebVitals();
+
+//BACKEND ROCKS
 
 ReactDOM.render(<App/>,document.querySelector("#root"));
---
+++ src/Client/constants/actionTypes.js
@@ -0,0 +1,4 @@
+export const ADD_ENTRY = 'Make a new entry'
+export const FETCH_ENTRIES = 'Get all entries'
+export const SET_NEW_ENTRY = 'Set the value of an entry'
+export const DELETE_ENTRY = 'Delete an entry'
